5bd33c4e02f5498a037f4b04aee04fb6b63af960
ahugla <ahugla@vmware.com>
2020-04-08 18:06:33 +0200

    Update vRCS_Tito_update.sh

--- Tito/8-avr-2020/vRCS_Tito_update.sh
+++ Tito/8-avr-2020/vRCS_Tito_update.sh
@@ -4,13 +4,34 @@
 #         - configure pour le distributed tracing  (prerequis python doit etre present sur le serveur)
 #		  - reconfigure wavefront avec les nouveaux inputs
 #
+
+#   Inputs:  
+#  		  - Tito_Version  (ex V2.4)
+#		  - PROXY_NAME    (ex wvfp.cpod-vrealizesuite.az-demo.shwrfr.com)
+#		  - PROXY_PORT    (ex 2878)
+#
+#	Appel :   ./vRCS_Tito_update.sh  Tito_Version PROXY_NAME PROXY_PORT
+#
+#
 #	Date :  8/04/2020 
 #	Owner:  Alexandre Hugla
 #
 
-systemctl stop httpd
 
 cd /tmp
+
+# Get Parameters
+Tito_Version=$1
+PROXY_NAME=$2
+PROXY_PORT=$3
+echo "Tito_Version="$Tito_Version
+echo "PROXY_NAME="$PROXY_NAME
+echo "PROXY_PORT="$PROXY_PORT
+
+
+
+systemctl stop httpd
+
 rm -rf /var/www/html
 
 git clone https://github.com/vmeoc/Tito.git  /var/www/html           
